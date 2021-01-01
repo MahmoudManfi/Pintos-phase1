dd400d498c1d273ce2c623f3819d28e87768f08d
Manfy <mahmoud.manfy159@gmail.com>
2021-01-02 01:32:01 +0200

    the first of phase 2

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -4,7 +4,24 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+static struct lock files_sys_lock;               /* lock for syschronization between files */
+
 static void syscall_handler (struct intr_frame *);
+
+void halt_wrapper();
+void exit_wrapper();
+void exec_wrapper();
+void wait_wrapper();
+void create_wrapper();
+void remove_wrapper();
+void open_wrapper();
+void filesize_wrapper();
+void read_wrapper();
+void write_wrapper();
+void seek_wrapper();
+void tell_wrapper();
+void close_wrapper();
+
 
 void
 syscall_init (void) 
@@ -15,6 +32,168 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
+  
+  void *esp = f->esp;
+
+  switch (get_int(esp))
+  {
+  case SYS_HALT:
+    halt_wrapper();
+    break;
+  
+  case SYS_EXIT:
+    exit_wrapper();
+    break;
+
+  case SYS_EXEC:
+    exec_wrapper();
+    break;
+
+  case SYS_WAIT:
+    wait_wrapper();
+    break;
+
+  case SYS_CREATE:
+    create_wrapper();
+    break;
+
+  case SYS_REMOVE:
+    remove_wrapper();
+    break;
+
+  case SYS_OPEN:
+    open_wrapper();
+    break;
+
+  case SYS_FILESIZE:
+    filesize_wrapper();
+    break;
+
+  case SYS_READ:
+    read_wrapper();
+    break;
+
+  case SYS_WRITE:
+    write_wrapper();
+    break;
+
+  case SYS_SEEK:
+    seek_wrapper();
+    break;
+
+  case SYS_TELL:
+    tell_wrapper();
+    break;
+
+  case SYS_CLOSE:
+    close_wrapper();
+    break;
+
+  default:
+    break;
+  }
+
   thread_exit ();
+}
+
+int 
+get_int(int** esp)
+{
+
+}
+
+char* 
+get_char_ptr(char*** esp) 
+{
+
+}
+
+void* 
+get_void_ptr(void*** esp)
+{
+
+}
+
+void 
+validate_void_ptr(const void* pt)
+{
+
+}
+
+void
+halt_wrapper()
+{
+
+}
+
+void
+exit_wrapper()
+{
+  
+}
+
+void
+exec_wrapper()
+{
+  
+}
+
+void
+wait_wrapper()
+{
+  
+}
+
+void
+create_wrapper()
+{
+  
+}
+
+void
+remove_wrapper()
+{
+  
+}
+
+void
+open_wrapper()
+{
+  
+}
+
+void
+filesize_wrapper()
+{
+  
+}
+
+void
+read_wrapper()
+{
+  
+}
+
+void
+write_wrapper()
+{
+  
+}
+
+void
+seek_wrapper()
+{
+  
+}
+
+void
+tell_wrapper()
+{
+  
+}
+
+void
+close_wrapper()
+{
+  
 }
--- userprog/syscall.h
+++ userprog/syscall.h
@@ -3,4 +3,16 @@
 
 void syscall_init (void);
 
+/* get int from the stack */
+int get_int(int** esp);
+
+/* get character pointer */
+char* get_char_ptr(char*** esp);
+
+/* get void pointer */
+void* get_void_ptr(void*** esp);
+
+/* check if the pointer is valid */
+void validate_void_ptr(const void* pt);
+
 #endif /* userprog/syscall.h */
